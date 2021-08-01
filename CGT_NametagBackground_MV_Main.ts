3d3d6bd2554481f3ea3d148b4a2acd79591b17cc
CG-Tespy <gbcabreracgt@gmail.com>
2021-08-01 14:34:59 -0400

    Fixed Spanish translation

--- CGT_NametagBackground_MV_Main.ts
+++ CGT_NametagBackground_MV_Main.ts
@@ -4,7 +4,7 @@
 *
 * @author CG-Tespy https://github.com/CG-Tespy
 * 
-* @help This is version 1.01.03 of this plugin. This relies on the YEP_MessageCore and
+* @help This is version 1.01.03a of this plugin. This relies on the YEP_MessageCore and
 * CGT_CoreEngine (v1.01.03+) plugins.
 * 
 * Other contributors:
@@ -58,7 +58,7 @@
 * @plugindesc Te deja usar un imágen entero como textura para el gráfico fondo de un gafete, en lugar de un window.png
 * @author CG-Tespy https://github.com/CG-Tespy
 * 
-* @help Versión 1.01.03 de este plugin. Esto necesita el YEP_MessageCore plugin y el 
+* @help Versión 1.01.03a de este plugin. Esto necesita el YEP_MessageCore plugin y el 
 * CGT_CoreEngine (v1.01.03+) plugin.
 * 
 * Otros colaboradores:
@@ -67,40 +67,47 @@
 * Si quieres editar este plugin, tal vez será mejor si lo haces por el fuente:
 * https://github.com/CG-Tespy/CGT_NametagBackground_MV
 *
-* @param Ancho del Gráfico
+* @param Graphic Width
+* @text Ancho del Gráfico
 * @type number
 * @min 1
 * @default 300
 * @desc Pone el ancho del gráfico del gafete. Ignorado si Queda el Gráfico al Gafete está activo.
 * 
-* @param Altura del Gráfico
+* @param Graphic Height
+* @text Altura del Gráfico
 * @type number
 * @min 1
 * @default 100
 * @desc Pone la altura del gráfico del gafete. Ignorado si Queda el Gráfico al Gafete está activo.
 * 
-* @param Opacidad del Gráfico
+* @param Graphic Opacity
+* @text Opacidad del Gráfico
 * @type number
 * @min 0
 * @max 255
 * @default 192
 * @desc Pone la opacidad del gráfico del gafete de una escala de 0 a 255.
 * 
-* @param Queda el Gráfico al Gafete
+* @param Fit Graphic to Nametag
+* @text Queda el Gráfico al Gafete
 * @type boolean
 * @default false
 * @desc Whether the name box graphic's dimensions should be scaled to the nametag's dimensions.
 * 
-* @param Grafico del Gafete
+* @param Nametag Graphic
+* @text Grafico del Gafete
 * @type file
 * @dir img/system
 * 
-* @param Compensación X de la Gráfica
+* @param Graphic X Offset
+* @text Compensación X de la Gráfica
 * @type number
 * @default 0
 * @desc Compensación posicional de la gráfica del gafete en el eje X.
 * 
-* @param Compensación Y de la Gráfica
+* @param Graphic Y Offset
+* @text Compensación Y de la Gráfica
 * @type number
 * @default 0
 * @desc Compensación posicional de la gráfica del gafete en el eje Y.
